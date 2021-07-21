e790268c70bf38e429eef831c599cc5931cfd9c5
Nick Earwood <nick.earwood@palmetto.com>
2021-07-21 15:22:16 -0500

    Update deps

--- test/index.js
+++ test/index.js
@@ -28,38 +28,28 @@ describe('socket.io-postgres', function () {
   it('broadcasts to rooms', function (done) {
     create(function (server1, client1) {
       create(function (server2, client2) {
-        create(function (server3, client3) {
           server1.on('connection', function (c1) {
             c1.join('woot');
           });
 
           server2.on('connection', function (c2) {
             // does not join, performs broadcast
-            c2.on('do broadcast', function () {
-              c2.broadcast.to('woot').emit('broadcast');
-            });
-          });
-
-          server3.on('connection', function (c3) {
-            // does not join, signals broadcast
-            client2.emit('do broadcast');
+          c2.broadcast.to('woot').emit('broadcast', { a: 'b' });
           });
 
           client1.on('broadcast', function () {
             client1.disconnect();
             client2.disconnect();
-            client3.disconnect();
-            setTimeout(done, 100);
+          done(); // This gets called as far as I can tell but the test still fails.
+
+          // setTimeout(function () {
+          // }, 100);
           });
 
           client2.on('broadcast', function () {
             throw new Error('Not in room');
           });
 
-          client3.on('broadcast', function () {
-            throw new Error('Not in room');
-          });
-        });
       });
     });
   });
